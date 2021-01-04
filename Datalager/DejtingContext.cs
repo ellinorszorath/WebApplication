7342dbaf0cbfd9a1194976dfe119fee4b1b4fec2
SebbeT99 <s.ternevid99@gmail.com>
2021-01-04 16:39:33 +0100

    ny login

--- Datalager/DejtingContext.cs
+++ Datalager/DejtingContext.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Datalager.Models;
 
 namespace Datalager
 {
-    public class DejtingContext : DbContext
+    public class DejtingContext : IdentityDbContext
     {
         public DejtingContext(DbContextOptions<DejtingContext> options) : base(options)
         {
