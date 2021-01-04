bd68f3d4f2a3391d7393cd4242a140897df3bd46
SebbeT99 <s.ternevid99@gmail.com>
2021-01-04 15:55:03 +0100

    Nytt projekt

---
+++ Datalager/DejtingContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Datalager.Models;
+
+namespace Datalager
+{
+    public class DejtingContext : DbContext
+    {
+        public DejtingContext(DbContextOptions<DejtingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Personer { get; set; }
+    }
+}
---
+++ Datalager/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datalager.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Namn { get; set; }
+        public DateTime Födelsedatum { get; set; }
+    }
+}
