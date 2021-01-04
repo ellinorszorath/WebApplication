fa0a6e6a1291fdc04fbfd985beb4108c11d74e2d
Slaskmeister <simon.99@gmail.com>
2021-01-04 11:58:31 +0100

    Registera

---
+++ WebApplication3/Models/Registrerad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Registrerad
+    {
+        public String Förnamn{ get; set; }
+        public String Efternamn { get; set; }
+        public int Ålder { get; set; }
+
+    }
+}
