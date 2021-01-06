ed6ff300b5e7deee4416c561dfad1f4214148780
SebbeT99 <s.ternevid99@gmail.com>
2021-01-06 13:04:37 +0100

    s

--- Dejtingsida/Models/Registrerad.cs
+++ Dejtingsida/Models/Registrerad.cs
@@ -9,10 +9,14 @@ namespace WebApplication3.Models
     public class Registrerad
     {
         [Key]
-        public int Id { get; set; }
-        public String Förnamn{ get; set; }
-        public String Efternamn { get; set; }
+
+        public String Användarnamn { get; set; }
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
         public DateTime Födelsedatum { get; set; }
+        public string Lösenord { get; set; }
+        public int Vänförfrågningar { get; set; }
+        public string ProfilbildUrl { get; set; }
 
     }
 }
