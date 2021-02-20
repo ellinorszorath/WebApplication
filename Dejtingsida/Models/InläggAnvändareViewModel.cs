e5b5de80b2be27383cef3c3d01d7243c88acedf4
Slaskmeister <simon.99@gmail.com>
2021-02-20 09:41:43 +0100

    Skapa inlägg div, ViewModel

---
+++ Dejtingsida/Models/InläggAnvändareViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datalager.Models;
+
+namespace Dejtingsida.Viewmodels
+{
+    public class InläggAnvändareViewModel
+    {
+        public Inlägg inlägg { get; set; }
+    }
+}
