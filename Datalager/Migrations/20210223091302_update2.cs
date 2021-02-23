4efc811946846911b212f528502d4bd91d5e45b6
SebbeT99 <s.ternevid99@gmail.com>
2021-02-23 10:16:44 +0100

    ruta för sök

---
+++ Datalager/Migrations/20210223091302_update2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datalager.Migrations
+{
+    public partial class update2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Visas",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Visas",
+                table: "AspNetUsers");
+        }
+    }
+}
